8cc62e8e78913363e5ac142d2178afa3a58d5c73
Subhash <subhash@siriam.in>
2024-03-15 17:45:16 +0530

    till pointer arthmetic

---
+++ pointer_example.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+
+    int num=10;
+    char ch='a';
+    float f_val=10.33;
+
+    int roll[]={5,6,8,12,56,12,36};
+
+    int *pt =roll;
+    
+    pt=pt + 3;
+
+    for(int i=0;i<sizeof(roll)/sizeof(int)/2+1;i++)
+    {
+        printf("%d\n",*(pt--));
+    }
+    
+
+
+
+    //printf("\n%f\n\n",*p_f);
+}
