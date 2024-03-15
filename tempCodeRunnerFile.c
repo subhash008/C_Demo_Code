23cf74a466f8e9705791179e40e90b2776568a06
Subhash <subhash@siriam.in>
2024-03-15 12:14:39 +0530

    first commit : nothing special

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void  main(){
+
+    int x= 12.0/5;
+
+    printf("%d\n\n", x);
+
+}
+
---
+++ test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void  main(){
+    
+    int a=15;
+    int b=20;
+    int c= a >> 2 ;
+    printf("result : %d\n\n",c);
+
+}
+
